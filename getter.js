a460d3132d615a6d17cf8d4023f67be2eaa9f8c3
Fauzi <fadli.zain@leson.id>
2020-04-02 13:13:39 +0700

    adding global data

--- getter.js
+++ getter.js
@@ -38,6 +38,15 @@ const main = async () => {
 }
 
 const getData = async (page = 1) => {
+    try {
+        getIdData();
+        getGlobalData();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getIdData =  (params) => {
     const toDate = moment.tz(moment(), 'Asia/Jakarta').startOf('hour').format();
     const fromDate = moment(toDate).subtract(6, 'hour').startOf('hour').format();
     const baseUrl = process.env.NEWSAPI_BASE_URL;
@@ -53,31 +62,64 @@ const getData = async (page = 1) => {
         pageSize: 100
     });
 
-    try {
-        const url = `${baseUrl}/everything?${params}`;
-        console.log(url);
-        const response = await axios.get(url);
-        if (response.data.status != "ok") {
-            throw("Fetch failed at url");
-        }
-        const articles = response.data.articles;
-        const result = articles.map(article => {
-            let slug = article.title;
-            slug = slug.replace(/[^a-zA-Z0-9 ]/g, "");
-            slug = slug.replace(/\s+/g, '-').toLowerCase();
-            article.slug = slug
-
-            if (!article.urlToImage) article.urlToImage = "https://semantic-ui.com/images/wireframe/image.png"
-            if (!article.author) article.author = article.source.name;
-            return article
-        }).sort((x, y) => new Date(x.publishedAt) - new Date(y.publishedAt));
-        storeData(result);
-    } catch (err) {
-        console.log(err);
+    const url = `${baseUrl}/everything?${params}`;
+    console.log(url);
+    const response = await axios.get(url);
+    if (response.data.status != "ok") {
+        throw("Fetch failed at url");
     }
+    const articles = response.data.articles;
+    const result = articles.map(article => {
+        let slug = article.title;
+        slug = slug.replace(/[^a-zA-Z0-9 ]/g, "");
+        slug = slug.replace(/\s+/g, '-').toLowerCase();
+        article.slug = slug
+
+        if (!article.urlToImage) article.urlToImage = "https://semantic-ui.com/images/wireframe/image.png"
+        if (!article.author) article.author = article.source.name;
+        return article
+    }).sort((x, y) => new Date(x.publishedAt) - new Date(y.publishedAt));
+    storeDataId(result);
 }
 
-const storeData = async (data) => {
+const getGlobalData =  (params) => {
+    const toDate = moment.tz(moment(), 'Asia/Jakarta').startOf('hour').format();
+    const fromDate = moment(toDate).subtract(6, 'hour').startOf('hour').format();
+    const baseUrl = process.env.NEWSAPI_BASE_URL;
+    const apiKey = process.env.NEWSAPI_API_KEY;
+    
+    const params = qs.stringify({
+        qInTitle: 'corona OR covid 19 OR covid19',
+        from: fromDate,
+        to: toDate,
+        apiKey: apiKey,
+        language: 'en',
+        sortBy: 'popularity',
+        page: page,
+        pageSize: 100
+    });
+
+    const url = `${baseUrl}/everything?${params}`;
+    console.log(url);
+    const response = await axios.get(url);
+    if (response.data.status != "ok") {
+        throw("Fetch failed at url");
+    }
+    const articles = response.data.articles;
+    const result = articles.map(article => {
+        let slug = article.title;
+        slug = slug.replace(/[^a-zA-Z0-9 ]/g, "");
+        slug = slug.replace(/\s+/g, '-').toLowerCase();
+        article.slug = slug
+
+        if (!article.urlToImage) article.urlToImage = "https://semantic-ui.com/images/wireframe/image.png"
+        if (!article.author) article.author = article.source.name;
+        return article
+    }).sort((x, y) => new Date(x.publishedAt) - new Date(y.publishedAt));
+    storeDataGlobal(result);
+}
+
+const storeDataId = async (data) => {
     try {
         console.log(data.length);
         if (data.length < 1) return;
@@ -86,6 +128,34 @@ const storeData = async (data) => {
         Promise.all(
             data.forEach((x) => {
                 return db.collection('articles-id').updateOne({
+                    slug: x.slug,
+                    url: x.url
+                }, {
+                    $set: x
+                }, {
+                    upsert: true
+                })
+            })
+        ).then(() => {
+            Promise.resolve(config.mongoClose());
+        });
+        // db.collection('articles-id').insertMany(data).then(async () => {
+        //     await config.mongoClose();
+        // });
+    } catch (err) {
+        throw err;
+    }
+}
+
+const storeDataGlobal = async (data) => {
+    try {
+        console.log(data.length);
+        if (data.length < 1) return;
+
+        const db = await config.mongoConnect();
+        Promise.all(
+            data.forEach((x) => {
+                return db.collection('articles-en').updateOne({
                     slug: x.slug,
                     url: x.url
                 }, {
